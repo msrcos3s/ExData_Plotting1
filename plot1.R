f2a63c4e1d8e82487789cef4942c247477641cd0
Marcos Medeiros <93498592+msrcos3s@users.noreply.github.com>
2021-12-29 10:52:20 -0300

    Add files via upload
    Plot 1

---
+++ plot1.R
@@ -0,0 +1,36 @@
+## Plot 1
+## subset from 2007-02-01 and 2007-02-02
+## histogram of global active power
+
+plot1 <- function(){
+        
+library(ggplot2)
+        
+## file in working directory
+        
+full_data <- read.table("./household_power_consumption.txt", stringsAsFactors = FALSE, header = TRUE, sep =";")
+
+## changing class of all variables to numeric
+
+full_data$Date <- as.Date(full_data$Date, format="%d/%m/%Y")
+full_data$Time <- format(full_data$Time, format="%H:%M:%S")
+full_data$Global_active_power <- as.numeric(full_data$Global_active_power)
+full_data$Global_reactive_power <- as.numeric(full_data$Global_reactive_power)
+full_data$Voltage <- as.numeric(full_data$Voltage)
+full_data$Global_intensity <- as.numeric(full_data$Global_intensity)
+full_data$Sub_metering_1 <- as.numeric(full_data$Sub_metering_1)
+full_data$Sub_metering_2 <- as.numeric(full_data$Sub_metering_2)
+full_data$Sub_metering_3 <- as.numeric(full_data$Sub_metering_3)
+
+
+## subsetting
+
+subsetdata <- subset(full_data, Date == "2007-02-01" | Date =="2007-02-02")
+
+## plotting
+
+png("plot1.png", width=500, height=500)
+hist(subsetdata$Global_active_power, col="red", border="black", main ="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency")
+
+dev.off()
+}
